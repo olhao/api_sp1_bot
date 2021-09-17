9474603fb24f0d3ad0d604c28e4a510eb5d630e8
Olha <orukshyna@gmail.com>
2021-09-17 22:36:44 +0200

    Modified logging; add try, raise and exept; use for loop instead of if-else

--- homework.py
+++ homework.py
@@ -1,7 +1,7 @@
 import logging
 import os
 import time
-from logging import FileHandler, StreamHandler
+from logging.handlers import RotatingFileHandler
 
 import requests
 import telegram
@@ -13,97 +13,77 @@ PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 PRAKTIKUM_URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
-current_timestamp = int(time.time()) - 86400  # для int(time.time()) падает с ошибкой: 86400 == 1 день
-
+HEADERS = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+STATUSES = {'rejected': 'К сожалению, в работе нашлись ошибки.',
+            'reviewing': 'Pабота взята в ревью.',
+            'approved': 'Ревьюеру всё понравилось, работа зачтена!'}
 
 bot = telegram.Bot(TELEGRAM_TOKEN)
 
 
-def file_logger():
-    logging.basicConfig(
-        level=logging.DEBUG,
-        filename='main.log',
-        format='%(asctime)s, %(levelname)s, %(message)s, %(name)s',
-        filemode='a',)
-
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
-    file_handler = FileHandler('main.log')
-    logger.addHandler(file_handler)
-
-
-def file_debug_logger():
-    file_logger()
-    logging.debug('Бот запущен')
-
-
-def file_info_logger():
-    file_logger()
-    logging.info('Сообщение отправлено')
-
-
-def file_error_logger():
-    file_logger()
-    logging.error('Бот не смог отправить сообщение')
-
-
-def stream_error_logger():
-    logging.basicConfig(
-        level=logging.ERROR,
-        filename='main.log',
-        format='%(asctime)s, %(levelname)s, %(name)s, %(message)s',
-        filemode='a',)
-
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.ERROR)
-    stream_handler = StreamHandler()
-    logger.addHandler(stream_handler)
-
-    logging.error('Бот не смог отправить сообщение')
-
-
 def get_homeworks(current_timestamp):
-    homework_statuses = requests.get(
-        PRAKTIKUM_URL,
-        headers={'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'},
-        params={'from_date': current_timestamp})
-    return homework_statuses.json()
+    try:
+        homework_statuses = requests.get(
+            PRAKTIKUM_URL,
+            headers=HEADERS,
+            params={'from_date': current_timestamp})
+        return homework_statuses.json()
+    except Exception:
+        logging.exception('Бот не смог отправить сообщение')
+        logging.error('Бот не смог отправить сообщение', exc_info=True)
 
 
 def parse_homework_status(homework):
     homework_name = homework['homework_name']
     homework = homework['status']
-    if homework == 'rejected':
-        verdict = 'К сожалению, в работе нашлись ошибки.'
-    elif homework == 'reviewing':
-        verdict = 'Pабота взята в ревью.'
-    else:
-        verdict = 'Ревьюеру всё понравилось, работа зачтена!'
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    for status, verdict in STATUSES.items():
+        try:
+            if homework == status:
+                return ('У вас проверили работу '
+                        f'"{homework_name}"!\n\n{verdict}')
+            raise Exception(f'Cтатуса {homework} домашней работы не найдено.')
+        except Exception:
+            logging.exception('Бот не смог отправить сообщение')
+            logging.error('Бот не смог отправить сообщение', exc_info=True)
 
 
 def send_message(message):
-    return bot.send_message(CHAT_ID, message)
+    try:
+        return bot.send_message(CHAT_ID, message)
+    except Exception:
+        logging.exception('Бот не смог отправить сообщение')
+        logging.error('Бот не смог отправить сообщение', exc_info=True)
 
 
 def main():
+    logging.basicConfig(
+        level=logging.DEBUG,
+        filename=os.path.expanduser(__file__ + '.log'),
+        format='%(asctime)s, %(levelname)s, %(message)s, %(name)s',
+        filemode='a',)
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    file_handler = RotatingFileHandler(os.path.expanduser(__file__ + '.log'),
+                                       maxBytes=50000000,
+                                       backupCount=5)
+    logger.addHandler(file_handler)
+    logging.debug('Бот запущен')
+    #  для int(time.time()) падает с ошибкой: 86400 == 1 день
     current_timestamp = int(time.time()) - 86400
-    file_debug_logger()
-
     while True:
         try:
-            homework = get_homeworks(current_timestamp=current_timestamp)['homeworks'][0]
+            homework = get_homeworks(
+                current_timestamp=current_timestamp)['homeworks'][0]
             message = parse_homework_status(homework)
             send_message(message)
-            file_info_logger()
-            time.sleep(20 * 60)  # Опрашивать раз в 20 минут
+            logging.info('Сообщение отправлено')
+            time.sleep(20 * 60)
 
         except Exception as e:
             message = f'Бот упал с ошибкой: {e}'
             bot.send_message(CHAT_ID, message)
-            stream_error_logger()
-            file_error_logger()
-            time.sleep(20 * 60)
+            logging.exception('Бот не смог отправить сообщение')
+            logging.error('Бот не смог отправить сообщение', exc_info=True)
 
 
 if __name__ == '__main__':
