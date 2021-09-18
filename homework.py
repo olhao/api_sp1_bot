0797c27000dc5ea233917b4ff678db2fde3e87c7
Olha <orukshyna@gmail.com>
2021-09-18 18:41:20 +0200

    Modified raise and parse homework_status_method

--- homework.py
+++ homework.py
@@ -14,7 +14,7 @@ TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 PRAKTIKUM_URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
-STATUSES = {'rejected': 'К сожалению, в работе нашлись ошибки.',
+VERDICTS = {'rejected': 'К сожалению, в работе нашлись ошибки.',
             'reviewing': 'Pабота взята в ревью.',
             'approved': 'Ревьюеру всё понравилось, работа зачтена!'}
 
@@ -27,38 +27,36 @@ def get_homeworks(current_timestamp):
             PRAKTIKUM_URL,
             headers=HEADERS,
             params={'from_date': current_timestamp})
+        if homework_statuses.json()["homeworks"] is None:
+            raise Exception(f'{homework_statuses}')
         return homework_statuses.json()
-    except Exception:
-        logging.exception('Бот не смог отправить сообщение')
-        logging.error('Бот не смог отправить сообщение', exc_info=True)
+    except Exception as exception:
+        print(f'{exception}')
 
 
 def parse_homework_status(homework):
     homework_name = homework['homework_name']
     homework = homework['status']
-    for status, verdict in STATUSES.items():
-        try:
-            if homework == status:
-                return ('У вас проверили работу '
-                        f'"{homework_name}"!\n\n{verdict}')
-            raise Exception(f'Cтатуса {homework} домашней работы не найдено.')
-        except Exception:
-            logging.exception('Бот не смог отправить сообщение')
-            logging.error('Бот не смог отправить сообщение', exc_info=True)
+
+    if homework in VERDICTS.keys():
+        verdict = VERDICTS[homework]
+        return ('У вас проверили работу '
+                f'"{homework_name}"!\n\n{verdict}')
+    raise IndexError(f'Статус работы {homework_name} не найден')
 
 
 def send_message(message):
     try:
         return bot.send_message(CHAT_ID, message)
     except Exception:
-        logging.exception('Бот не смог отправить сообщение')
-        logging.error('Бот не смог отправить сообщение', exc_info=True)
+        # здесь имя не нужно logging.exception уже выводит всю необходимую информацию с описание ексепшина
+        logging.exception(f'Бот не смог отправить сообщение')
 
 
 def main():
     logging.basicConfig(
         level=logging.DEBUG,
-        filename=os.path.expanduser(__file__ + '.log'),
+        filename=__file__ + '.log',
         format='%(asctime)s, %(levelname)s, %(message)s, %(name)s',
         filemode='a',)
     logger = logging.getLogger(__name__)
@@ -69,7 +67,7 @@ def main():
     logger.addHandler(file_handler)
     logging.debug('Бот запущен')
     #  для int(time.time()) падает с ошибкой: 86400 == 1 день
-    current_timestamp = int(time.time()) - 86400
+    current_timestamp = int(time.time()) - 24*60*60
     while True:
         try:
             homework = get_homeworks(
@@ -77,14 +75,13 @@ def main():
             message = parse_homework_status(homework)
             send_message(message)
             logging.info('Сообщение отправлено')
-            time.sleep(1800)
 
-        except Exception as e:
-            message = f'Бот упал с ошибкой: {e}'
-            bot.send_message(CHAT_ID, message)
+        except Exception as exception:
+            message = f'Ошибка: {exception}'
+            send_message(message)
             logging.exception('Бот не смог отправить сообщение')
             logging.error('Бот не смог отправить сообщение', exc_info=True)
-            time.sleep(1800)
+        time.sleep(20*60)
 
 
 if __name__ == '__main__':
